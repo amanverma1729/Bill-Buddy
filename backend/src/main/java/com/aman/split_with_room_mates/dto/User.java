e866a19e6aea1f02b2bf211c86622df19b2241d4
“[Aman <amanverma0202004@gmail.com>
2026-09-01 20:23:03 +0530

    Update backend API URL

--- backend/src/main/java/com/aman/split_with_room_mates/dto/User.java
+++ backend/src/main/java/com/aman/split_with_room_mates/dto/User.java
@@ -12,7 +12,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -29,8 +28,7 @@ import lombok.Setter;
 public class User {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "employee_seq")
-	@SequenceGenerator(name = "employee_seq", sequenceName = "employee_sequence", allocationSize = 1, initialValue = 9001)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String email;
